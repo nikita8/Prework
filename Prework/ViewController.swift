e4dc5874db347c91f875b61876b5dd48e348aad5
Nikita Acharya <nikita.acharya8@gmail.com>
2020-12-28 16:06:00 -0500

    Make bill field focused on app load

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billField.becomeFirstResponder()
     }
 
     @IBAction func calculateTip(_ sender: Any) {
