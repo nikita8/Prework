0e6541accb8dded022fd8bcf5825551f4100c2b8
Nikita Acharya <nikita.acharya8@gmail.com>
2020-12-28 16:34:58 -0500

    Add additional features

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,25 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         billField.becomeFirstResponder()
+        let locale = Locale.current
+        let currencySymbol = String(locale.currencySymbol!)
+        tipLabel.text = String(format: "\(currencySymbol)%.2f", 0.00)
+        totalLabel.text = String(format: "\(currencySymbol )%.2f", 0.00)
     }
 
     @IBAction func calculateTip(_ sender: Any) {
         // Get the initial bill amount
         let bill = Double(billField.text!) ?? 0
         
-        // Calculate the tip and total
-        let tipPercentages = [0.15, 0.18, 0.20]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
-        // Update the tip and total labels
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        let locale = Locale.current
+        let currencySymbol = String(locale.currencySymbol!)
+        if bill == 0{
+            tipLabel.text = String(format: "\(currencySymbol)%.2f", 0.00)
+            totalLabel.text = String(format: "\(currencySymbol )%.2f", 0.00)
+        }else{
+            // Calculate the tip and total
+            let tipPercentages = [0.15, 0.18, 0.20]
+            let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+            let total = bill + tip
+            
+            // Update the tip and total labels
+           
+            
+            tipLabel.text = String(format: "\(currencySymbol)%.2f", locale: locale, tip)
+            totalLabel.text = String(format: "\(currencySymbol )%.2f", locale: locale, total)
+        }
     }
     
     @IBAction func onTap(_ sender: Any) {
